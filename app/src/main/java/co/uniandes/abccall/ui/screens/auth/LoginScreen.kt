386d064b32429ddc5c20371bb015d3818cf7af51
jorcasca <jorgeeliecercastao@gmail.com>
2024-09-30 18:39:06 -0500

    feat(Login): implement screen"

--- app/src/main/java/co/uniandes/abccall/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/co/uniandes/abccall/ui/screens/auth/LoginScreen.kt
@@ -1,6 +1,10 @@
 package co.uniandes.abccall.ui.screens.auth
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -9,25 +13,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import co.uniandes.abccall.R
-import co.uniandes.abccall.ui.navigation.Screen
 import co.uniandes.abccall.ui.navigation.goMain
 
 @Composable
 fun LoginScreen(navController: NavController) {
     val emailState = remember { mutableStateOf("") }
     val passwordState = remember { mutableStateOf("") }
-    val passwordVisibility = remember { mutableStateOf(false) }
+    val isButtonEnabled by derivedStateOf {
+        emailState.value.isNotBlank() && passwordState.value.isNotBlank()
+    }
 
     Column(
         modifier = Modifier.padding(16.dp),
@@ -36,14 +43,22 @@ fun LoginScreen(navController: NavController) {
         Text(
             modifier = Modifier.padding(top = 55.dp, bottom = 83.dp ),
             text = stringResource(id = R.string.app_name).uppercase(),
-            color = MaterialTheme.colorScheme.secondary
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleLarge
         )
 
         OutlinedTextField(
             value = emailState.value,
             onValueChange = { emailState.value = it },
-            label = { Text(stringResource(id = R.string.email)) },
-            placeholder = { Text(stringResource(id = R.string.email)) },
+            label = { Text(
+                    text = stringResource(id = R.string.email),
+                    style = MaterialTheme.typography.bodyLarge
+            ) },
+            placeholder = { Text(
+                text = stringResource(id = R.string.email),
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Gray
+            ) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
             modifier = Modifier
                 .fillMaxWidth()
@@ -53,18 +68,67 @@ fun LoginScreen(navController: NavController) {
         OutlinedTextField(
             value = passwordState.value,
             onValueChange = { passwordState.value = it },
-            label = { Text(stringResource(id = R.string.password)) },
-            placeholder = { Text(stringResource(id = R.string.password)) },
-            visualTransformation = if (passwordVisibility.value) VisualTransformation.None else PasswordVisualTransformation(),
+            label = { Text(
+                text = stringResource(id = R.string.password),
+                style = MaterialTheme.typography.bodyLarge
+            ) },
+            placeholder = { Text(
+                text = stringResource(id = R.string.password),
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Gray
+            ) },
+            visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
         )
 
         Button(
             onClick = { navController.goMain() },
-            modifier = Modifier.padding(top = 16.dp)
+            enabled = isButtonEnabled
         ) {
-            Text(text = stringResource(id = R.string.access))
+            Text(
+                text = stringResource(id = R.string.access).uppercase(),
+                style = MaterialTheme.typography.headlineLarge
+            )
         }
+
+        Row(
+            modifier = Modifier.padding(vertical = 42.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.no_account),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = stringResource(id = R.string.sign_up),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.clickable {
+                    Toast.makeText(
+                        navController.context,
+                        navController.context.getString(R.string.not_implemented),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            )
+        }
+
+        Text(
+            text = stringResource(id = R.string.forgot_password),
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.clickable {
+                Toast.makeText(
+                    navController.context,
+                    navController.context.getString(R.string.not_implemented),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
+
     }
+
 }
--- app/src/main/java/co/uniandes/abccall/ui/theme/Type.kt
+++ app/src/main/java/co/uniandes/abccall/ui/theme/Type.kt
@@ -10,25 +10,31 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp,
+        lineHeight = 25.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W700,
+        fontSize = 16.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W700,
+        fontSize = 18.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
     )
-    */
+
 )
