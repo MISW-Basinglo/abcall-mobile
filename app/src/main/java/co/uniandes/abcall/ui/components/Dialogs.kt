a28fe989874a54944802129c3fcce6a918cca1e4
jorge.castanov <jorge.castanov@perficient.com>
2024-11-20 13:09:02 -0500

    hotfix(Issue): expand dialog

--- app/src/main/java/co/uniandes/abcall/ui/components/Dialogs.kt
+++ app/src/main/java/co/uniandes/abcall/ui/components/Dialogs.kt
@@ -1,8 +1,8 @@
 package co.uniandes.abcall.ui.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -24,15 +24,19 @@ fun MinimalDialog(onDismissRequest: () -> Unit = {}, text: String) {
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
-            Text(
-                text = text.ifEmpty { stringResource(R.string.empty_issue_solution) },
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .heightIn(min = 100.dp, max = 400.dp)
                     .padding(20.dp)
-                    .wrapContentSize(Alignment.Center),
-                textAlign = TextAlign.Center,
-            )
+                    .verticalScroll(rememberScrollState()),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = text.ifEmpty { stringResource(R.string.empty_issue_solution) },
+                    textAlign = TextAlign.Center,
+                )
+            }
         }
     }
 }
-
